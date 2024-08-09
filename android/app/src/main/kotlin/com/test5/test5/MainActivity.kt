c0dd7628006dc4ac74560357cb6d56303c093f27
joelsaji <joelsajiputhenpurakkal@gmail.com>
2024-08-09 14:13:07 +0530

    all done

---
+++ android/app/src/main/kotlin/com/test5/test5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.test5.test5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,572 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => NewsSearchScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.newspaper, size: 100, color: Colors.white),
+            SizedBox(height: 20),
+            Text(
+              'Instant News',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 36,
+                fontWeight: FontWeight.w700,
+                letterSpacing: 1.5,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Discover the world through us',
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 18,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewsSearchScreen extends StatefulWidget {
+  @override
+  _NewsSearchScreenState createState() => _NewsSearchScreenState();
+}
+
+class _NewsSearchScreenState extends State<NewsSearchScreen> {
+  final TextEditingController _controller = TextEditingController();
+  bool _showSearchField = false;
+
+  Future<List<Map<String, dynamic>>> _fetchAndFilterNews(String query) async {
+    String apiKey = '4b5b82fa46734dea986685f599bc7beb';
+    String url =
+        'https://newsapi.org/v2/everything?qInTitle=$query&from=2024-07-09&language=en&sortBy=publishedAt&apiKey=3104ea1278484a65baa6cf11bca98f8a';
+
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final List<dynamic> articles = data['articles'];
+
+      final List<Map<String, dynamic>> articlesWithImages = articles
+          .where((article) => article['urlToImage'] != null && article['urlToImage'].isNotEmpty)
+          .map((e) => e as Map<String, dynamic>)
+          .toList();
+
+      final List<Map<String, dynamic>> validArticles = (await Future.wait(
+        articlesWithImages.map((article) async {
+          final imageUrl = article['urlToImage'];
+          bool imageCanBeLoaded = await _canLoadImage(imageUrl);
+          return imageCanBeLoaded ? article : null;
+        }),
+      )).whereType<Map<String, dynamic>>().toList();
+
+      return validArticles;
+    } else {
+      print('Failed to fetch news');
+      return [];
+    }
+  }
+
+  Future<bool> _canLoadImage(String url) async {
+    try {
+      final response = await http.get(Uri.parse(url));
+      return response.statusCode == 200;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  void _searchNews(String query) async {
+    final List<Map<String, dynamic>> filteredArticles = await _fetchAndFilterNews(query);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => NewsResultsScreen(articles: filteredArticles),
+      ),
+    );
+  }
+
+  void _onSearchPressed() {
+    _searchNews(_controller.text);
+  }
+
+  void _onCategoryButtonPressed(String category) {
+    _searchNews(category);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final buttonHeight = screenHeight * 0.15;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.newspaper, size: 30, color: Colors.white),
+            SizedBox(width: 8),
+            Text(
+              'Instant News',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.white),
+            onPressed: () {
+              setState(() {
+                _showSearchField = !_showSearchField;
+              });
+            },
+          ),
+        ],
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.white),
+          onPressed: () => Scaffold.of(context).openDrawer(),
+        ),
+        bottom: _showSearchField
+            ? PreferredSize(
+          preferredSize: Size.fromHeight(50.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: 'Search for news',
+                hintStyle: TextStyle(color: Colors.black54),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              onSubmitted: (value) => _onSearchPressed(),
+            ),
+          ),
+        )
+            : null,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              title: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginScreen(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text('Login'),
+              ),
+            ),
+            ListTile(
+              title: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignupScreen(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text('Sign Up'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildCategoryButton('Sports', buttonHeight),
+              SizedBox(height: 16),
+              _buildCategoryButton('Technology', buttonHeight),
+              SizedBox(height: 16),
+              _buildCategoryButton('Health', buttonHeight),
+              SizedBox(height: 16),
+              _buildCategoryButton('Business', buttonHeight),
+              SizedBox(height: 16),
+              _buildCategoryButton('Latest', buttonHeight),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryButton(String category, double height) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: height,
+      child: ElevatedButton(
+        onPressed: () => _onCategoryButtonPressed(category),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        child: Text(category),
+      ),
+    );
+  }
+}
+
+class NewsResultsScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> articles;
+
+  NewsResultsScreen({required this.articles});
+
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top News', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22)),
+        backgroundColor: Colors.black,
+      ),
+      body: articles.isEmpty
+          ? Center(
+        child: Text(
+          'No results found',
+          style: TextStyle(color: Colors.white, fontSize: 18, fontStyle: FontStyle.italic),
+        ),
+      )
+          : ListView.builder(
+        itemCount: articles.length,
+        itemBuilder: (context, index) {
+          final article = articles[index];
+          return Card(
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            color: Colors.black,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(
+                  article['urlToImage'],
+                  width: double.infinity,
+                  height: 200,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) {
+                    return Container(
+                      color: Colors.black,
+                      height: 200,
+                      child: Center(
+                        child: Icon(Icons.error, color: Colors.white),
+                      ),
+                    );
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    article['title'] ?? 'No Title',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20,
+                      letterSpacing: 1.1,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    article['source']['name'] ?? 'Unknown Source',
+                    style: TextStyle(color: Colors.white70, fontSize: 16),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => NewsDetailScreen(article: article),
+                          ),
+                        );
+                      },
+                      child: Text('Read More'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[800],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class NewsDetailScreen extends StatelessWidget {
+  final Map<String, dynamic> article;
+
+  NewsDetailScreen({required this.article});
+
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          article['title'] ?? 'No Title',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (article['urlToImage'] != null)
+              Image.network(
+                article['urlToImage'],
+                width: double.infinity,
+                height: 250,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                    color: Colors.black,
+                    height: 250,
+                    child: Center(
+                      child: Icon(Icons.error, color: Colors.white),
+                    ),
+                  );
+                },
+              ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                article['title'] ?? 'No Title',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                article['source']['name'] ?? 'Unknown Source',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.white70,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                article['content'] ?? 'No Content',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () => _launchURL(article['url']),
+                  child: Text('Read Full Article'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[800],
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                    textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Handle login logic
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+              ),
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Handle sign-up logic
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+              ),
+              child: Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
